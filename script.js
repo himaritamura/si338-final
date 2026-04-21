0d872439fea6a6d23f259b9f0582acef47e409ec
Himari Tamura <himari@umich.edu>
2026-04-20 22:28:01 -0400

    flips!'

--- script.js
+++ script.js
@@ -29,18 +29,28 @@ function Recommend(event) {
     song_grid.innerHTML = "";
     // Create song-card, add to song-grid
     for (let i = 0; i < recommendations.length; ++i) {
-        let song_html = "<div class='song-card'> <h2>";
-        song_html += recommendations[i].track_name;
-        song_html += "</h2> <h3>";
-        song_html += recommendations[i].artists;
-        song_html += "</h3> <h4>";
-        song_html += recommendations[i].album_name;
-        song_html += "</h4> <h5> <img src='icons/time.svg' alt='Duration icon' class='card-icon'> Duration (ms): ";
-        song_html += recommendations[i].duration_ms;
-        song_html += "</h5> <h5> <img src='icons/energy.svg' alt='Energy icon' class='card-icon'> Energy: ";
-        song_html += recommendations[i].energy;
-        song_html += "</h5> </div>";
-
+        let song_html = "<div class='song-card'> <div class='card-inner'> <div class='card-front'> <h2>" + 
+        recommendations[i].track_name + 
+        "</h2> <h3>" + 
+        recommendations[i].artists + 
+        "</h3> <h4>" + 
+        recommendations[i].album_name + 
+        "</h4> <h5> <img src='icons/time.svg' alt='Duration icon' class='card-icon'> Duration (ms): " + 
+        recommendations[i].duration_ms + 
+        "</h5> <h5> <img src='icons/energy.svg' alt='Energy icon' class='card-icon'> Energy: " + 
+        recommendations[i].energy + 
+        "</h5> </div> <div class='card-back'> <h6> Explicit: " + 
+        recommendations[i].explicit + 
+        "</h6> <h6>Popularity: " +
+        recommendations[i].popularity + 
+        "</h6> <h6>Danceability: " +
+        recommendations[i].danceability + 
+        "</h6> <h6>Key: " +
+        recommendations[i].key + 
+        "</h6> <h6>Tempo: " +
+        recommendations[i].tempo + 
+        "</h6> </div> </div> </div>";
+        
         song_grid.innerHTML += song_html;
     }
 }
